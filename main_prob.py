38b62b88e1677ba14e7974161609c600991b2e02
Kevin.F <Ken365X@outlook.com>
2020-03-26 14:05:32 +0800

    2020/3/26-14:04

--- main_prob.py
+++ main_prob.py
@@ -1,5 +1,8 @@
 # coding=utf-8
 
+import json
+import os
+
 from IPID_HCSC.collision_prob import Collision_Prob
 
 
@@ -9,8 +12,17 @@ if __name__ == '__main__':
     attack_bind_if = 'eth0'
     own_ip_prefix = '10.10.0.0'
 
-    collision = Collision_Prob(attack_target_server=server_ip, attack_target_network=own_ip_prefix,
+    collision = Collision_Prob(attack_target_server=server_ip, attack_target_network=own_ip_prefix, net_type='C',
                                bind_iface_name=attack_bind_if, block_size=200, num_thread=8, verbose=True)
     collision.run()
     collision.wait_for_res()
 
+    jstr = json.dumps({'res': collision.result})
+    path = './prob_res'
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    filelist = os.listdir(path)
+
+    with open(path + '/prob_res_' + str(len(filelist))+ '.json', 'w') as f:
+        f.write(jstr)
