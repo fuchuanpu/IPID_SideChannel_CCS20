6608b81412079a338ef647bd56106c87c3e41605
Kevin.F <Ken365X@outlook.com>
2020-04-01 08:19:08 +0800

    2020/4/1-8:17

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 
 git checkout clould
 
 sudo apt-get install python3-pip
-pip3 install scapy
+pip3 install scapy==2.4.2
 pip3 install psutil
 pip3 install websockets
-
-
-# git clone https://fuchuanpu:Fcp6198168@github.com/fuchuanpu/TCP-exploit.git
